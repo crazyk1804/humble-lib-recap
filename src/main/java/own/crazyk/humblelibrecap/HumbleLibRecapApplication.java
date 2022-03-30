b794c49944ba78f84c5358ee564a90a2eb784251
crazyk <hans1804@daum.net>
2022-03-30 11:40:41 +0900

    Initial commit

---
+++ src/main/java/own/crazyk/humblelibrecap/HumbleLibRecapApplication.java
@@ -0,0 +1,13 @@
+package own.crazyk.humblelibrecap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HumbleLibRecapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HumbleLibRecapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/own/crazyk/humblelibrecap/HumbleLibRecapApplicationTests.java
@@ -0,0 +1,13 @@
+package own.crazyk.humblelibrecap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HumbleLibRecapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
